c055e5f067eef9a475f874b33e7cb8b033a10949
JustSmn <ssi0601@mail.ru>
2025-03-02 19:43:52 +0700

    main.go

--- qrbot/main.go
+++ qrbot/main.go
@@ -8,15 +8,12 @@ import (
 )
 
 func main() {
-	// Замени на свой токен
 	botToken := "7819453922:AAHMYUiRh1M3RwXW_aeNJOZn2xgSdP0nlM4"
 
-	// Создаем бота
 	qrBot, err := bot.NewBot(botToken)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	// Запускаем бота
 	qrBot.Start()
 }
